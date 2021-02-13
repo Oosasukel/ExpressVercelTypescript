485613e11d882c9cfcaf15c9cb8093e8cdea3d06
Oosasukel <rodrigo_gonn@hotmail.com>
2021-02-13 19:27:55 -0300

    sum test

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { sum } from './sum';
 
 const app = express();
 
@@ -7,7 +8,7 @@ app.get('/api', (req, res) => {
   const path = `/api/item/${itemId}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="qwe">${path}</a>`);
+  res.end(`Hello! Go to item: <a href="${path}">${itemId}</a>`);
 });
 
 app.get('/api/test', (req, res) => {
@@ -17,6 +18,14 @@ app.get('/api/test', (req, res) => {
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
+});
+
+app.get('/api/sum/:num1/:num2', (req, res) => {
+  const { num1, num2 } = req.params;
+
+  const result = sum(Number(num1), Number(num2));
+
+  res.end(`${num1} + ${num2} = ${result}`);
 });
 
 export default app;
